a3e1c8731945d8abc1264944092ac175b556c3aa
focoemcanto-pixel <focoemcanto@gmail.com>
2026-02-10 16:54:42 -0300

    Create sidebar.php file with sidebar widget content

---
+++ harmonics/sidebar.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Sidebar Widget Content
+ */
+
+function display_sidebar() {
+    echo '<div class="sidebar-widget">';
+    echo '<h3>Sidebar Title</h3>';
+    echo '<p>Sidebar content goes here. You can add HTML, links, or anything you like.</p>';
+    echo '</div>';
+}
+?>
